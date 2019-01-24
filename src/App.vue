b340e06b8e2b1a2c73b58e0c0ba64815ab30d5de
Tom <syntraprogrammeurs@gmail.com>
2019-01-24 11:18:55 +0100

    register initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,146 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col col-6">
+          <div class="card">
+            <h4 class="card-header bg-dark text-white">
+              Register
+            </h4>
+          </div>
+          <div class="card-body">
+            <form action="">
+              <div class="form-group">
+                <label for="">User</label>
+                <input
+                        id="user_user"
+                        v-model="user.user"
+                        class="form-control"
+                        type="text"
+                        
+                >
+              </div>
+              <div class="form-group">
+                <label for="">Email</label>
+                <input
+                        id="user_email"
+                        v-model="user.email"
+                        class="form-control"
+                        type="email">
+              </div>
+              <div class="form-group">
+                <label for="">Password</label>
+                <input
+                        id="user_password"
+                        v-model="user.password"
+                        class="form-control"
+                        type="password">
+              </div>
+              <div class="form-group">
+                <label for="">Verjaardag</label>
+                <input
+                        id="user_verjaardag"
+                        v-model="user.verjaardag"
+                        class="form-control"
+                        type="date">
+              </div>
+             
+              <div class="form-group">
+                <label for="">Biografie</label>
+                <textarea
+                        id="user.biografie"
+                        v-model="user.biografie"
+                        cols="30"
+                        rows="10"
+                        class="form-control">
+                
+                </textarea>
+                
+              </div>
+              <div class="form-group">
+                <label for="">Geslacht</label>
+                <select class="form-control" v-model="user.geslacht">
+                  <option
+                          v-for="gender in genders"
+                          :value="gender"
+                          
+                  >
+                    {{ gender }}
+                  </option>
+                </select>
+  
+              </div>
+              <div class="form-group">
+               <div class="form-check">
+                 <label for="" class="form-check-label">
+                   <input
+                           v-model="user.receiveNieuwsbrief"
+                           type="checkbox"
+                           class="form-check-input">
+                   Nieuwsbrief?
+                 </label>
+               </div>
+              </div>
+              <div class="form-group">
+                <button class="btn btn-primary">
+                  Register
+                </button>
+                <button
+                        @click="reset"
+                        class="btn btn-danger">
+                  Reset
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+    
+    data(){
+      return{
+          user:{
+              user: '',
+              email: '',
+              password: '',
+              verjaardag:'',
+              biografie:'',
+              geslacht:'',
+              receiveNieuwsbrief:true
+          },
+          genders:[
+              'man',
+              'vrouw',
+              'ietsanders'
+          ]
+      }
+    },
+    methods:{
+      reset(){
+          this.user = {
+              user: '',
+              email: '',
+              password: '',
+              verjaardag:'',
+              biografie:'',
+              geslacht:'',
+              receiveNieuwsbrief:true
+          }
+      },
+        register(){
+        
+        }
+    }
+    
+
 }
 </script>
 
